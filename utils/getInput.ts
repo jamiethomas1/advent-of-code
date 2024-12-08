2d815d140fc594b89ce3d8f890970d3e685c95e6
Jamie Thomas <jamiewthomas13@gmail.com>
2024-12-08 12:47:24 +0000

    Add getTestInput function

--- utils/getInput.ts
+++ utils/getInput.ts
@@ -3,3 +3,7 @@ const { readFileSync } = require("fs");
 export default function readInputFile(year: string, day: string): string {
   return readFileSync(`challenges/${year}/${day}/input.txt`, "utf-8");
 }
+
+export function getTestInput(year: string, day: string): string {
+  return readFileSync(`challenges/${year}/${day}/test.txt`, "utf-8");
+}
